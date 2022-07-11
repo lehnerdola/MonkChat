0ff4d183f72b7ab3cf90b44fa3e6636643791328
Leticia <ln9458143@gmail.com>
2022-07-11 19:48:16 -0300

    api login/cadastro 100%

--- api/src/index.js
+++ api/src/index.js
@@ -11,6 +11,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+
+// conf dos endpoints
 server.use(usuarioController);
 
 server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`));
